27e0be53ff0a1c217847bc1f27f366e23963fbea
QwazyWabbit <gljoy@mac.com>
2017-02-18 13:32:37 -0800

    Eliminate dependency on __linux__ and __sun__

--- q_shared.h
+++ q_shared.h
@@ -17,7 +17,7 @@
 	#include <stdlib.h>
 	#include <time.h>
 
-	#if (defined _M_IX86 || defined __i386__) && !defined C_ONLY && !defined __sun__
+#if defined _M_IX86 && !defined C_ONLY
 	#define id386	1
 	#else
 	#define id386	0
