dd4a3099e2ddfa56fe8468da10cd43d7ee2914da
QwazyWabbit <gandalf.2004@gmail.com>
2017-10-02 13:45:42 -0700

    Add startup banner and some project settings and .gitignore settings.

--- g_save.c
+++ g_save.c
@@ -128,7 +128,7 @@ is loaded.
 
 void InitGame (void)
 {
-	gi.dprintf ("==== InitGame ====\n");
+	gi.dprintf ("==== InitGame ECTF "EXPERT_VERSION" built "__DATE__" ====\n");
 
 	gun_x = gi.cvar ("gun_x", "0", 0);
 	gun_y = gi.cvar ("gun_y", "0", 0);
